b99ecd94c9d18f81e879a50400835e7cb6c475d8
Tung Du <dinhtungdu@gmail.com>
2024-02-24 20:36:50 +0700

    add: break tailwind classes into groups in the editor

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => 'efe45b2ff9365f99d409');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '8023c884c96ef2cc4367');
--- src/editor.js
+++ src/editor.js
@@ -15,31 +15,77 @@ if (
 	classList = createTailwindcssContext( tailwind.config ).getClassList();
 }
 
+// Classify classes into categories based on prefix
+function classify( classes ) {
+	const classified = {
+		root: [],
+	};
+	classes.forEach( ( c ) => {
+		// split the css class into prefix and rest by the last colon
+		// e.g. "md:hover:text-white" => ["md:hover", "text-white"]
+		const [ p, ...rest ] = c.split( /:(?!.*:)/ );
+		if ( rest.length === 0 ) {
+			classified.root.push( p );
+			return;
+		}
+		if ( ! classified[ p ] ) {
+			classified[ p ] = [];
+		}
+		classified[ p ].push( rest[ 0 ] );
+	} );
+	return classified;
+}
+
+function classNameToClasses( className ) {
+	return classify(
+		className
+			? [ ...new Set( className.split( ' ' ) ) ].filter( Boolean )
+			: []
+	);
+}
+
+function classesToClassName( classes ) {
+	return Object.entries( classes )
+		.map( ( [ p, c ] ) => {
+			return c.map( ( c ) => `${ p }:${ c }` ).join( ' ' );
+		} )
+		.join( ' ' )
+		.replace( /root:/g, '' );
+}
+
 const withGutailbergControls = createHigherOrderComponent( ( BlockEdit ) => {
 	return ( props ) => {
+		const classes = classNameToClasses( props.attributes.className );
 		return (
 			<>
 				<BlockEdit key="edit" { ...props } />
 				<InspectorControls>
-					<PanelBody>
-						<FormTokenField
-							label={ __( 'CSS classes', 'gutailberg' ) }
-							onChange={ ( tokens ) =>
-								props.setAttributes( {
-									className: [ ...new Set( tokens ) ]
-										.sort()
-										.join( ' ' ),
-								} )
-							}
-							value={ [
-								...new Set(
-									( props.attributes?.className || '' ).split(
-										' '
-									)
-								),
-							].filter( Boolean ) }
-							suggestions={ classList }
-						/>
+					<PanelBody title="Tailwind CSS Classes">
+						{ Object.entries( classes ).map(
+							( [ prefix, prefixClasses ] ) => (
+								<FormTokenField
+									key={ prefix }
+									label={ prefix }
+									onChange={ ( tokens ) => {
+										const newClasses = {
+											...classes,
+											[ prefix ]: [
+												...new Set( tokens ),
+											].sort(),
+										};
+
+										props.setAttributes( {
+											className:
+												classesToClassName(
+													newClasses
+												),
+										} );
+									} }
+									value={ prefixClasses }
+									suggestions={ classList }
+								/>
+							)
+						) }
 					</PanelBody>
 				</InspectorControls>
 			</>
